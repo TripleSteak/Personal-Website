04a19ab5b435c6d164593aa955ebdfb60cfa8db8
TripleSteak <simono768@gmail.com>
2023-06-12 23:31:13 -0400

    updated resume and rewrote BioPanel description

--- src/components/BioPanel/constants.tsx
+++ src/components/BioPanel/constants.tsx
@@ -12,12 +12,14 @@ export const EnglishText = () => {
             drive for bettering the world around me through creation is what keeps me going each and
             every morning.<br/><br/>
 
-            Hi! I'm <span className="text-bold">Simon Ou</span>, an ambitious student currently studying software
-            engineering at the University of Waterloo. Recently, I've been working on projects involving serverless
-            applications, web development, socket infrastructure, cloud architecture, and game development.<br/><br/>
+            Hi! I'm <span className="text-bold">Simon Ou</span>. I'm currently attending full-time studies at the
+            University of Waterloo and working part-time as a full-stack software engineer. I love tinkering with
+            software and have experience with serverless applications, web development, socket infrastructure,
+            cloud architecture, and game development.<br/><br/>
 
-            Never hesitate to <a className="plain-link" href="mailto:simono768@gmail.com">contact
-            me</a>! I'm always down to chat and always down to learn.
+            Three things you should never forget about me– I love my family, I love my friends, and I love learning!
+            Befriend me, <a className="plain-link" href="mailto:simono768@gmail.com">teach me something</a>, and I
+            will stay by your side and do whatever I can to turn our wildest dreams into a reality!
         </p>
     )
 };
@@ -29,14 +31,14 @@ export const FrenchText = () => {
             quelque truc de nouveau. Le besoin irrépressible d'employer la création pour améliorer
             le monde autour de moi est exactement ce qui me propulse chaque matin.<br/><br/>
 
-            Salut! Je m'appelle <span className="text-bold">Simon Ou</span>, un étudiant ambitieux qui s'est inscrit
-            au programme d'ingénierie logicielle à <a className="plain-link" href="https://uwaterloo.ca/">l'Université
-            de Waterloo</a>. Depuis peu, je travaille sur les projets qui se concernent avec les applications sans
-            serveur, le développement Web, les résaux de sockets, l'architecture du cloud, et la conception des jeux.
-            <br/><br/>
+            Salut! Je m'appelle <span className="text-bold">Simon Ou</span>. Je fais actuellement mes études à plein
+            temps à l'Université de Waterloo et travaille à temps partiel comme ingénieur logiciel full-stack. Ça me dit
+            de bricoler les logiciels et j'ai de l'expérience avec les applications serverless, le développement Web,
+            les résaux de sockets, l'architecture du cloud, et la conception des jeux.<br/><br/>
 
-            N'hésitez jamais à <a className="plain-link" href="mailto:simono768@gmail.com">me contacter</a>! Ça me dit
-            de bavarder, ça me dit d'apprendre.
+            Il faut que vous n'oubliiez jamais ça: j'adore ma famille, j'adore mes amis, et j'adore apprendre!
+            Devenez mon ami, <a className="plain-link" href="mailto:simono768@gmail.com">enseignez-moi quelque chose</a>,
+            et je resterai à vos côtés et ferai tout ce que je pourrai pour transformer nos rêves effrénés en réalité!
         </p>
     )
 };
